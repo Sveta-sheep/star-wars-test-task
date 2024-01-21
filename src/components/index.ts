ac3fdf7cd1e451535d574c29f11f693223dd2618
Svitlana <svitlana.vysotska@zealicsolutions.com>
2024-01-21 17:08:44 +0200

    feat: add search for character

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,7 @@ export { ErrorFallbackUI } from './ErrorFallbackUI';
 export { CharacterList } from './CharacterList';
 export { Title } from './Title';
 export { Loader } from './Loader';
+export { SearchInput } from './SearchInput';
 
 export { CharacterItem } from './CharacterItem/CharacterItem';
 export { Pagination } from './Pagination/Pagination';
